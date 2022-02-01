7601a141fd5c5b79c1dcbbdf0bbbd4eff478cfba
Anvar Lapasov <lapasovanvar2908@gmail.com>
2022-02-01 11:43:33 +0300

    PUT request is finished

--- src/controller/eventsController.js
+++ src/controller/eventsController.js
@@ -1,7 +1,6 @@
 const GET = (req, res) => {
     try {
         const {id} = req.params
-
         let events = req.select('events')
         events.map(e => {
             delete e.organizer_status
@@ -24,7 +23,40 @@ const POST = (req, res) => {
 }
 
 const PUT = (req, res) => {
+    try {
+        const { eventId, seen, eventStatus} = req.body
+        let events = req.select('events')
+        if(!eventId){
+            throw new Error("eventId must be provided")
+        }
+        let find = events.find(e => e.event_id == eventId)
+        if(!find){
+            throw new Error("There is no such event!")
+        }
+        if(typeof eventStatus != 'undefined' && ![1, 2, 3].includes(eventStatus)){
+            throw new Error("eventStatus must be valid!")
+        }
+        if(typeof seen != 'number' && seen || seen < 0){
+            throw new Error("seen must be valid!")
+        }
+        if(seen || eventStatus){
+            events.map(e => {
+                if(e.event_id == eventId){
+                    e.seen = seen ? seen : e.seen
+                    e.status = eventStatus? eventStatus : e.status
+                }
+            })
+            req.insert('events', events)
+        } 
 
+        res.status(201).json({
+            message: "The event has been successfully edited!",
+            status: 201
+        })
+    } catch (error) {
+        // return next(error)
+        res.json({ message: error.message })
+    }
 }
 
 
--- src/router/events.js
+++ src/router/events.js
@@ -6,6 +6,6 @@ const router = Router()
 router.get('/events', controllers.GET)
 router.get('/events/:id', controllers.GET)
 router.post('/events', controllers.POST)
-router.post('/events', controllers.PUT)
+router.put('/events', controllers.PUT)
 
 export default router
